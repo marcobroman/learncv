153d5a37a1e2bfa3ac8a89e257a4129635e76d93
Marco roman <roman.bmarco@gmail.com>
2023-06-29 14:46:29 -0500

    masks for video

---
+++ detect.py
@@ -0,0 +1,40 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
+
+# load the image
+image = cv2.imread(args["image"])
+image = cv2.resize(image,(0,0),fx=0.2,fy=0.3)
+
+
+# define the list of boundaries
+boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+]
+
+i = 0
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	# create NumPy arrays from the boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+	
+
+	# find the colors within the specified boundaries and apply the mask
+	mask = cv2.inRange(image, lower, upper)
+	output = cv2.bitwise_and(image, image, mask = mask)
+	cv2.imwrite("img/rgb/image" + str(i) + ".jpg",output)
+	i +=1
+	
+    # show the images
+	cv2.imshow("images", np.hstack([image, output]))
+	cv2.waitKey(0)
--- read.py
+++ read.py
@@ -1,6 +1,57 @@
-import cv2 as cv
+import cv2 
+import numpy as np
 
-#read img
-img = cv.imread("img/duck.JPG")
-cv.imshow("duck",img)
-cv.waitKey(0)
+def mouseRGB(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN: #checks mouse left button down condition
+        colorsB = frame[y,x,0]
+        colorsG = frame[y,x,1]
+        colorsR = frame[y,x,2]
+        colors = frame[y,x]
+        # print("Blue: ",colorsB)
+        # print("Green: ",colorsG)
+        # print("Red: ",colorsR)
+        print("BGR======: ",str(colors).replace(" ",","))
+        # print("Coordinates of pixel: X: ",x,"Y: ",y)
+        print()
+
+
+# read img
+# img = cv2.imread("m.jpg",0)
+# # img = cv2.resize(img,(400,400))
+# img = cv2.resize(img,(0,0),fx=0.3,fy=0.3)
+# img = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
+# cv2.imwrite("newimg.jpg",img)
+# cv2.imshow("duck",img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+capture = cv2.VideoCapture(0)
+cv2.namedWindow('ori')
+cv2.setMouseCallback('ori',mouseRGB)
+
+#red boundary
+# low, high =[104, 96, 180], [100, 67, 255]
+#orange
+low, high =[59,57,57], [82,81,77] # 142,166,209 - 179,188,226
+
+#blues
+# low, high = [50, 0, 0], [255, 255, 180]
+
+lower = np.array(low, dtype = "uint8")
+upper = np.array(high, dtype = "uint8")
+#RGB
+
+while True:
+    isTrue,frame = capture.read()
+    mask = cv2.inRange(frame, lower, upper)
+    output = cv2.bitwise_and(frame, frame, mask = mask)
+
+    cv2.imshow("ori",frame)
+    cv2.imshow("mask",mask)
+    cv2.imshow("out",output)
+    if cv2.waitKey(20) & 0xFF==ord('d'):
+        break
+
+
+capture.release()
+cv2.destroyAllWindows()
